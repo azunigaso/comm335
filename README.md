cb278c810447c743a608d1b6dcc552f9b58d57b2
Alejandro <azunigas@masonlive.gmu.edu>
2019-12-03 17:49:36 -0500

    UI upgrades

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-E-sports team based company. 
+Merge Gaming is an E-sports team based company with a gamer forward facing mentality.
+
+Main functionalities of the page include:
+-Shopping Cart
+-Contact us that sends email to user in confirmation and follow up
--- app.js
+++ app.js
@@ -1,29 +1,45 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const path = require('path');
 
 // Tells express to use bodyParser for post requests variables
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
 // Sets render method default to EJS
 app.set("view engine", "ejs");
-
+app.use("/public", express.static(path.join(__dirname, 'public')));
 //Landing Route
-app.get("/",function(req,res){
-res.render("landing.ejs");
+app.get("/", function(req, res) {
+  res.render("landing.ejs");
 });
 
-app.get("/about", function(req,res){
-res.render("about.ejs");
+app.get("/about", function(req, res) {
+  res.render("about.ejs");
 });
 
-app.get("/services", function(req,res){
-res.render("services.ejs");
+app.get("/merch", function(req, res) {
+  res.render("merch.ejs");
 });
 
-app.get("/contact", function(req,res){
-res.render("contact.ejs");
+app.get("/contact", function(req, res) {
+  res.render("contact.ejs");
 });
 
-app.listen(3000,function(){
+app.post("/contact", function(req,res){
+
+
+
+  // redirect back to contact page
+
+  res.redirect("/contact");
+
+
+});
+
+app.listen(3000, function() {
   console.log("Merge server active");
 });
